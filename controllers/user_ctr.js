9bfa894e49d0443dc8f6b04d7345de52ba2da0be
BatutaNkuma <saelbatita@gmailcom>
2022-12-11 18:15:46 +0100

    add controller

---
+++ controllers/user_ctr.js
@@ -0,0 +1,90 @@
+const { PrismaClient } = require('@prisma/client');
+const Notification = require('./../middlewares/notification/prisma.notification');
+const { genSaltSync, compareSync, hashSync } = require('bcryptjs');
+const Auth = require('../middlewares/auth/auth.basic');
+const Notification = require('../middlewares/notification');
+const Prisma = new PrismaClient();
+const { SignToken } = new Auth();
+const { _error, _success } = new Notification();
+
+
+class ControllerUser {
+    //s'inscrire
+    async singUp(req, res) {
+        try {
+            const { name, email, password, avatar } = req.body;
+            //hashage de mot de passe
+            const salt = genSaltSync(10);
+            const passwordHash = hashSync(password, salt);
+            if (!passwordHash) throw "Cryptage crash";
+            //verification de l'email existance
+            const emailExist = await Prisma.user.findFirst({ where: { email: email } });
+            if (emailExist) throw new Error("This email address exists");
+            // si l'adress email n'existe pas alors enregistre l'utilisateur
+            const model = await Prisma.user.create({
+                data: {
+                    name: name,
+                    email: email,
+                    password: passwordHash,
+                    avatar: avatar
+                }
+            });
+            _success(res, 201, { response: model, tokenKey: SignToken(model.id) });
+        } catch (error) {
+            _error(res, 401, error.message);
+        }
+    }
+
+    //se connecter
+    async loginIn(req, res) {
+        try {
+            const { password, email } = req.body;
+            if (!password || !email) throw new Error("Please fill in all fields");
+            const emailExist = await Prisma.user.findFirst({ where: { email: email } });
+            if (!emailExist) throw new Error("Your password or email is invalid");
+            if (!compareSync(password, emailExist.password)) throw new Error("Your password or email is invalid");
+            _success(res, 201, { response: phoneExist, tokenKey: SignToken(phoneExist.id) });
+        } catch (error) {
+            _error(res, 401, error.message);
+        }
+    }
+
+    // read all client
+    async findAll(req, res) {
+        try {
+            const model = await Prisma.user.findMany();
+            _success(res, 200, model);
+        } catch (error) {
+            _error(res, 400, error.message);
+        }
+    }
+    // find for id
+    async findId(req, res) {
+        try {
+            const model = await Prisma.user.findFirst({ where: { id: req.params.id } });
+            _success(res, 200, model);
+        } catch (error) {
+            _error(res, 400, error.message);
+        }
+    }
+    //update for id client
+    async updateId(req, res) {
+        try {
+            const model = await Prisma.user.update({ where: { id: req.params.id }, data: req.body });
+            _success(res, 200, model);
+        } catch (error) {
+            _error(res, 400, error.message);
+        }
+    }
+    //delete client for id
+    async deleteId(req, res) {
+        try {
+            const model = await Prisma.user.delete({ where: { id: req.params.id } });
+            _success(res, 200, model);
+        } catch (error) {
+            _error(res, 400, error.message);
+        }
+    }
+}
+
+module.exports = ControllerUser;
--- middlewares/notification/prisma.notification.js
+++ middlewares/notification/prisma.notification.js
@@ -1,11 +1,11 @@
 class NotificationPrisma {
     //send success message
-    static async success(res, status = 200, message) {
+    async _success(res, status = 200, message) {
         return res.status(200).json({ status: status, message: message });
     }
 
     //send error message 
-    static async error(res, status = 400, message) {
+    async _error(res, status = 400, message) {
         if(message.meta){
             return res.status(400).json({ status: status, message: message.meta.target });
         }else if (message.message) {
@@ -15,4 +15,4 @@ class NotificationPrisma {
     }
 }
 
-export default NotificationPrisma;
+module.exports = NotificationPrisma;
