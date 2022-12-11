506499bf04dfb3d2fb560accfec3c5377230e27a
BatutaNkuma <saelbatita@gmailcom>
2022-12-11 16:48:51 +0100

    add notification sms orange

---
+++ middlewares/notification/orange.sms.js
@@ -0,0 +1,81 @@
+const https = require("https");
+const request = require("request");
+
+class OrangeSMS {
+    /**
+     * 
+     * @param {Basic **************==} tokenBasic Secret ket 
+     * @param {+243 000000000} phoneNumber number customer Orange
+     */
+    constructor(tokenBasic, phoneNumberOrange) {
+        this.tokenBasic = tokenBasic;
+        this.phoneNumberOrange = phoneNumberOrange;
+    }
+    /**
+     * 
+     * @param {+243 000000000} desinateur Number Phone Customer
+     * @param {Mbote Mokili} message Message For Customer
+     */
+    sender(phoneCustomer, message) {
+        credentials = this.tokenBasic;
+        var postData = "";
+        postData += "grant_type=client_credentials";
+        var options = {
+            host: 'api.orange.com',
+            path: '/oauth/v3/token'
+        };
+        options['method'] = 'POST';
+        options['headers'] = {
+            'Authorization': credentials,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        };
+        var req = https.request(options, function (response) {
+            response.setEncoding('utf8');
+            var responseData = '';
+            response.on('data', function (data) { responseData += data; });
+            response.on('end', function () {
+                var result = JSON.parse(responseData);
+                this._send(result.access_token, phoneCustomer, message);
+            });
+        })
+            .on('error', function (e) { });
+        req.write(postData);
+        req.end();
+    }
+
+    _send(token, phone, messagee) {
+        var receveirr = "tel:" + phone;
+        var senders = "tel:" + this.phoneNumberOrange;
+        var headers = {
+            'Authorization': "Bearer " + token,
+            'Content-Type': 'application/json'
+        };
+        var body = {
+            outboundSMSMessageRequest: {
+                address: receveirr,
+                senderAddress: senders,
+                outboundSMSTextMessage: {
+                    message: messagee
+                }
+            }
+        };
+        var options = {
+            uri: `https://api.orange.com/smsmessaging/v1/outbound/tel:${this.phoneNumberOrange}/requests`,
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        };
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 201) {
+                return response.statusCode;
+            }
+            else {
+                console.error(error);
+            }
+        })
+    }
+}
+
+
+export default OrangeSMS;
